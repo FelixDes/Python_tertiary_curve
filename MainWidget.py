ea57d84b65ccade1fff07737efbbcd9fcdd9eeb2
FelixDes <vip.zatebe@mail.ru>
2022-04-05 21:11:36 +0300

    New Gui

--- MainWidget.py
+++ MainWidget.py
@@ -3,11 +3,12 @@ import WindowPresenter
 import sys
 import webbrowser
 
-from PyQt5 import QtGui
+from PyQt5 import QtGui, uic
 from PyQt5.QtCore import QSize, QRegExp
 from PyQt5.QtGui import QRegExpValidator
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QScrollArea, QLabel, QSpinBox, \
-    QTableWidget, QComboBox, QStyledItemDelegate, QLineEdit, QTableWidgetItem, QDialog, QHBoxLayout, QVBoxLayout
+    QTableWidget, QComboBox, QStyledItemDelegate, QLineEdit, QTableWidgetItem, QDialog, QHBoxLayout, QVBoxLayout, \
+    QMainWindow
 
 PADDING = 10
 
@@ -17,32 +18,13 @@ WINDOW_SIZE = QSize(800, 800)
 INPUT_CONTAINER_SIZE = QSize(WINDOW_SIZE.width() // 2 - 2 * PADDING, 325)
 
 
-class MainWidget(QWidget):
+class MainWidget(QMainWindow):
     font = QtGui.QFont("ttf", 16)
 
     def __init__(self):
         super().__init__()
-        main_layout = QHBoxLayout()
-        left_layout = QVBoxLayout()
-        right_layout = QVBoxLayout()
+        uic.loadUi("ui.ui", self)
 
-        main_layout.addLayout(left_layout)
-        main_layout.addLayout(right_layout)
-
-        self.scroll_area = QScrollArea()
-
-        right_layout.addWidget(self.scroll_area)
-
-        menu_layout = QHBoxLayout()
-
-        self.curve_type_selector = QComboBox(self)
-        self.run_button = QPushButton(self)
-        menu_layout.addWidget(self.curve_type_selector)
-        menu_layout.addWidget(self.run_button)
-        left_layout.addLayout(menu_layout)
-
-        self.setLayout(main_layout)
-        self.set_listeners()
 
     def set_listeners(self):
         pass
