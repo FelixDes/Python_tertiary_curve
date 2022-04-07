d3d34e92df93611cd5169686ab1e3625eb01ac6d
quicklybly <quickly.tawer@gmail.com>
2022-04-07 13:40:45 +0300

    initialization of mpl

--- Plot3dModel.py
+++ Plot3dModel.py
@@ -8,14 +8,22 @@ class Plot3dModel:
     curve_types = ["Saddle", "Ellipsoid"]
 
     def __init__(self, grid_size, shift):
-        pass
+        self.fig = Figure()
+        self.canvas = FigureCanvas(self.fig)
+        self.x = np.arange(grid_size[0], grid_size[1], shift)
+        self.y = np.arange(grid_size[0], grid_size[1], shift)
+        self.x, self.y = np.meshgrid(self.x, self.y)
+
 
     def get_canvas(self, curve_index, *coefficients):
         match curve_index:
             case 0:
-                pass
+                saddle = SaddleCurve(*coefficients)
+                z = saddle.get_z(self.x, self.y)
+                self.fig.add_subplot(111, projection='3d').plot_surface(self.x, self.y, z)
+                return self.canvas
             case 1:
-                pass
+                # ellipsis =
 
 
 class Curve:
@@ -24,7 +32,7 @@ class Curve:
         self.b = b
         self.c = c
 
-    def get_z(self):
+    def get_z(self, x, y):
         return 0
 
 
@@ -32,13 +40,13 @@ class SaddleCurve(Curve):
     def __init__(self, a, b, c=None):
         super().__init__(a, b, c)
 
-    def get_z(self):
-        pass
+    def get_z(self, x, y):
+        return (((x ** 2) / (2 * self.a ** 2)) - ((y ** 2) / (2 * self.b ** 2)))
 
 
 class EllipsoidCurve(Curve):
     def __init__(self, a, b, c):
         super().__init__(a, b, c)
 
-    def get_z(self):
-        pass
+    def get_z(self, x, y):
+        return np.sqrt((1 - (x ** 2) / (self.a ** 2) - (y ** 2) / (self.b ** 2)) / (self.c ** 2))
